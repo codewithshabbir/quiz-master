0dc5531e8b6cfed85010eb0efc676b27205dfc63
Muhammad Shabbir <110288732+muhammadshabbir07@users.noreply.github.com>
2025-01-08 03:52:03 +0500

    some design add

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-import { quizData } from './data.js';
+import { quizData } from "./data.js";
 
 console.log(quizData);
 
+let quizCardsWrapper = document.getElementById("quiz-cards-wrapper");
 
-let quizCardsWrapper = document.getElementById('quiz-cards-wrapper');
+quizCardsWrapper.innerHTML = "";
 
-quizCardsWrapper.innerHTML = '';
-
-quizData.forEach(quiz => {
-    quizCardsWrapper.innerHTML += `
+quizData.forEach((quiz) => {
+  quizCardsWrapper.innerHTML += `
         <div class="bg-gray-800 rounded-lg shadow-md shadow-gray-600 p-4 py-8 flex flex-col justify-between">
             <div class="h-28 rounded-md flex justify-center items-center">
                 <img src="${quiz.quizLogoSrc}" alt="${quiz.quizTitle}" class="h-16"> <!-- Dynamically setting image source -->
@@ -19,7 +18,7 @@ quizData.forEach(quiz => {
                     <p class="text-gray-400">${quiz.quizQuestions.length} question(s)</p> <!-- Dynamically setting question count -->
                 </div>
                 <div class="flex justify-between items-center mt-4">
-                    <button class="bg-slate-700 text-white py-2 px-4 rounded-full hover:bg-slate-600">Start Quiz</button>
+                    <a href="./questions.html" class="bg-slate-700 text-white py-2 px-4 rounded-full hover:bg-slate-600">Start Quiz</a>
                 </div>
             </div>
         </div>
